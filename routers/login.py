036475f606668f73865eed020218b40c59cd906c
Cycyes <92714336+Cycyes@users.noreply.github.com>
2023-08-14 21:40:21 -0700

    Update login.py

--- routers/login.py
+++ routers/login.py
@@ -23,44 +23,52 @@ def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
-@router.get("/login")
-async def login():
-    code = 0
-    msg = "Please use POST method to login."
-    data = {}
-    return {
-        "code": code,
-        "msg": msg,
-        "data": data
-    }
+class CustomException(HTTPException):
+    def __init__(self, status_code, detail):
+        super().__init__(status_code=status_code, detail=detail)
 
 @router.post("/login")
 async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-    code = 0
-    msg = "登录成功"
-    data = {}
-
-    db_user = crud.get_user(db=db, stu_id=form_data.username)
-    if not db_user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="用户不存在",
-            headers={"WWW-Authenticate": "Bearer"},
+    try:
+        db_user = crud.get_user(db=db, stu_id=form_data.username)
+        if not db_user:
+            raise CustomException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="用户不存在",
+            )
+        if not verify_password(form_data.password, db_user.password):
+            raise CustomException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="密码错误",
+            )
+        
+        access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        access_token = create_access_token(
+            data={"sub": db_user.stu_id}, expires_delta=access_token_expires 
         )
-    if not verify_password(form_data.password, db_user.password):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="密码错误",
-            headers={"WWW-Authenticate": "Bearer"},
+        
+        data = {
+            'access_token': access_token,
+            'token_type': 'bearer'
+        }
+        
+        return {
+            "code": 0,
+            "msg": "登录成功",
+            "data": data
+        }
+    except SQLAlchemyError as e:
+        raise CustomException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="数据库错误",
         )
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": db_user.stu_id}, expires_delta=access_token_expires 
-    )
-    data['access_token'] = access_token
-    data['token_type'] = 'bearer'
-    return {
-        "code": code,
-        "msg": msg,
-        "data": data
-    }
+    except PyJWTError as e:
+        raise CustomException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="令牌生成失败",
+        )
+    except Exception as e:
+        raise CustomException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="发生未知错误",
+        )
