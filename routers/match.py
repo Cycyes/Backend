963bcc151525c588c9e04054c210a49bf323c928
Cycyes <92714336+Cycyes@users.noreply.github.com>
2023-08-14 22:42:20 -0700

    Update match.py

--- routers/match.py
+++ routers/match.py
@@ -6,11 +6,52 @@ from sqlalchemy.orm import Session
 from pydantic import BaseModel
 from passlib.context import CryptContext
 import time
+from typing import Dict
 
 router = APIRouter()
 
 class SocketId(BaseModel):
     socketId: str
+
+# 重构：获取匹配状态
+def get_match_status(current_user_id: str, db: Session) -> Dict[str, str]:
+    data = {"match": ""}
+
+    waiting_list = crud.get_all_waitings(db=db)
+    current_index = None
+
+    for i, waiting_user in enumerate(waiting_list):
+        if waiting_user.stu_id == current_user_id:
+            current_index = i
+            break
+    
+    if current_index is not None and current_index >= 1:
+        data["match"] = waiting_list[current_index - 1].stu_id
+    elif current_index is not None and current_index < len(waiting_list) - 1:
+        data["match"] = waiting_list[current_index + 1].stu_id
+
+    return data
+
+# 查询匹配状态
+@router.get("/queryMatch")
+async def queryMatch(db: Session = Depends(get_db), current_user: models.User = Depends(get_current_user)):
+
+    data = get_match_status(current_user.stu_id, db)
+
+    if data["match"] != "":
+        # 更新匹配状态并创建匹配记录
+        matched_user = data["match"]
+        crud.delete_wait(db=db, stu_id=current_user.stu_id)
+        crud.delete_wait(db=db, stu_id=matched_user)
+
+        t = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        db_match = crud.create_match(db=db, id0=current_user.stu_id, id1=matched_user, time=t, is_end=0)
+
+        return {"code": 0, "msg": "success", "data": data}
+    else:
+        return {"code": 0, "msg": "匹配失败", "data": data}
+
+
 
 # 结束匹配
 @router.post("/endMatch")
@@ -61,15 +102,6 @@ async def queryMatch(db: Session = Depends(get_db), current_user: models.User = 
         return {"code": 0, "msg": "success", "data": data}
     else:
         return {"code": 0, "msg": "匹配失败", "data": data}
-    '''
-        db_match = crud.get_match_by_id1(db=db, id1=current_user.stu_id)
-        if db_match:
-            data["match"] = db_match.id0
-            return {"code": 0, "msg": "success", "data": data}
-        else:
-            return {"code": 0, "msg": "匹配失败", "data": data}
-    '''
-        
 
 # 更新socketId
 @router.post("/match/uploadSocketId")
